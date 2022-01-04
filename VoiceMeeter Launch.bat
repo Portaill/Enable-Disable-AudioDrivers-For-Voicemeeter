d65494cfd3017da0a9574c8ef9d2bbe4bd6893a6
Portaill <33294313+Portaill@users.noreply.github.com>
2022-01-04 21:20:06 +0100

    Update VoiceMeeter Launch.bat

--- VoiceMeeter Launch.bat
+++ VoiceMeeter Launch.bat
@@ -10,8 +10,8 @@ SoundVolumeView.exe /enable "VoiceMeeter VAIO3 Output" & SoundVolumeView.exe /en
     
 Start "macro" /b "C:\Program Files (x86)\VB\Voicemeeter\VoicemeeterMacroButtons.exe" & call "C:\Program Files (x86)\VB\Voicemeeter\voicemeeter8.exe" 
      
-SoundVolumeView.exe /SwitchDefault "VoiceMeeter VAIO3 Input" "Headphone" all
-SoundVolumeView.exe /SwitchDefault "VoiceMeeter VAIO3 Output" "Headset Microphone" all
+SoundVolumeView.exe /SwitchDefault "VoiceMeeter VAIO3 Input" "---" all
+SoundVolumeView.exe /SwitchDefault "VoiceMeeter VAIO3 Output" "---" all
     taskkill /im VoicemeeterMacroButtons.exe /f 
         nircmd regsetval binary "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\MTCUVC" "EnableMtcUvc" "1" 
     SoundVolumeView.exe /disable "VoiceMeeter VAIO3 Input" & SoundVolumeView.exe /disable "VoiceMeeter Input" & SoundVolumeView.exe /disable "VoiceMeeter Aux Input"
